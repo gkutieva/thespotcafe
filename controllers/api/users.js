c5720c4f08fd1767f28b5cfabe697fd64bc5e521
gkutieva <73370477+gkutieva@users.noreply.github.com>
2021-10-04 16:10:36 -0700

    added some components

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -9,18 +9,23 @@ module.exports = {
 };
 
 function checkToken(req, res) {
-  // req.user puts a smile on your face
+  // req.user will always be there for you when a token is sent
   console.log('req.user', req.user);
   res.json(req.exp);
 }
 
 async function create(req, res) {
   try {
-    // Add the user to the db
+    // Add the user to the database
     const user = await User.create(req.body);
+    // token will be a string
     const token = createJWT(user);
+    // Yes, we can use res.json to send back just a string
+    // The client code take this into consideration
     res.json(token);
   } catch (err) {
+    // Client will check for non-2xx status code 
+    // 400 = Bad Request
     res.status(400).json(err);
   }
 }
@@ -29,18 +34,13 @@ async function login(req, res) {
   try {
     const user = await User.findOne({ email: req.body.email });
     if (!user) throw new Error();
-    const match = await bcrypt.compare(req.body.password, user.password);
-    if (match) {
-      const token = createJWT(user);
-      res.json(token);
-    } else {
-      throw new Error();
-    }
-  } catch {
+    await bcrypt.compare(req.body.password, user.password);
+    const token = createJWT(user);
+    res.json(token);
+  } catch () {
     res.status(400).json('Bad Credentials');
   }
 }
-
 
 /*-- Helper Functions --*/
 
@@ -49,6 +49,6 @@ function createJWT(user) {
     // data payload
     { user },
     process.env.SECRET,
-    { expiresIn: '24h'}
+    { expiresIn: '24h' }
   );
 }
